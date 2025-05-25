015fc5fd7a7fad55fa2b32b6cb39abe4c77c876e
gopinath <gopinaths2301@gmail.com>
2025-05-25 11:47:04 +0530

    homepage UI

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,47 @@
-import { Button } from '@/components/ui/button'
+import CompanionCard from '@/components/CompanionCard'
+import CompanionList from '@/components/CompanionList'
+import CTA from '@/components/CTA'
+import { recentSessions } from '@/constants'
 
 const Page = () => {
   return (
-    <>
-    <div className='underline decoration-orange-500'>Page
-    </div>
-    <Button>Button</Button>
-    </>
+    <main>
+    <h1 className='text-2xl underline'>Popular Components</h1>
+    <section className='home-section'>
+    <CompanionCard 
+      id ="123" 
+      name ="Neura the Brainy Explorer"
+      topic = "Neural Network of the Brain"
+      subject = "science"
+      duration = {45}
+      color = "#ffda6e"
+    />
+    <CompanionCard 
+      id ="1234" 
+      name ="County the Number wizard"
+      topic = "Neural Network of the Brain"
+      subject = "Maths"
+      duration = {30}
+      color = "#e5d0ff"
+    />
+    <CompanionCard 
+      id ="12345" 
+      name ="Verba the Verbal Explorer"
+      topic = "Language"
+      subject = "English"
+      duration = {45}
+      color = "#BDE7FF"
+    />
+    </section>
+    <section className='home-section'>
+      <CompanionList 
+        title = "Recently completed sessions"
+        companions ={recentSessions}
+        classNames="w-2/3 max-lg:w-full "
+      />
+      <CTA />
+    </section>
+    </main>
 
   )
 
